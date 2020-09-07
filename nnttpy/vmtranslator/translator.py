5ef7cc8140199e680079fefd32f188b68e7744d9
Ryosuke Nagumo <ryosuke.nagumo125@gmail.com>
2020-09-07 22:17:42 +0900

    Add translator class

---
+++ nnttpy/vmtranslator/translator.py
@@ -0,0 +1,62 @@
+
+from typing import Union, List
+
+import pathlib
+
+from nnttpy import vmtranslator
+
+
+class VMTranslator:
+    """Translator for VM code."""
+
+    def __init__(self):
+
+        self._parser = vmtranslator.VMParser()
+        self._writer = vmtranslator.VMCodeWriter()
+
+    def translate(self, path: Union[str, pathlib.Path]) -> List[str]:
+        """Translate given VM codes.
+
+        Args:
+            path (str or pathlib.Path): Path to .vm file or folder containing
+                multiple .vm files.
+
+        Returns:
+            code (list of str): Translated assemble code.
+
+        Raises:
+            ValueError: If given path specifies a single file and its suffix is
+                not '.vm'.
+        """
+
+        input_path = pathlib.Path(path)
+        if input_path.is_dir():
+            files = list(input_path.glob("*.vm"))
+        else:
+            if input_path.suffix != ".vm":
+                raise ValueError(f"Expected .vm file, but given {input_path}.")
+            files = [input_path]
+
+        for p in files:
+            with p.open("r") as f:
+                lines = f.readlines()
+            self._parser.code = lines
+
+            while True:
+                if self._parser.is_invalid():
+                    pass
+                elif self._parser.is_arithmetic():
+                    self._writer.write_arithmetic(self._parser.command)
+                elif self._parser.is_pushpop():
+                    self._writer.write_pushpop(
+                        self._parser.command, self._parser.arg1,
+                        self._parser.arg2)
+                else:
+                    raise NotImplementedError
+
+                try:
+                    self._parser.advance()
+                except RuntimeError:
+                    break
+
+        return self._writer.code
